fd9cba5821f51b14d3e718c052c56c2a3e7689d9
Gabriel Vital <gabrielvital@multi360digital.com.br>
2024-12-26 10:34:03 -0300

    first commit

---
+++ app/Http/Controllers/SeriesController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+
+class SeriesController extends Controller
+{
+    public function index()  
+    { 
+        $series = DB::select('SELECT FROM series;');
+
+        return view('series.index',compact('series'));
+    }
+
+    public function create()
+    {
+        return view('series.create');
+    }
+
+    public function store (Request $request)
+    {
+        $nomeSerie = $request->input('nome');
+
+        if (DB::insert('INSERT INTO series (nome) VALUES (?)',[$nomeSerie])) {
+            return "OK";
+        } else {
+            return "Deu erro";
+        }
+    }
+}
+
+
---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> {{$title}} - Controle de Séries </title>
+    <link rel="stylesheet" href="{{ asset('/css/app.css') }}"">
+</head>
+<body>
+    <div class="container">
+        <h1>{{ $title }} </h1>
+        {{ $slot }}
+    </div>
+</body>
+
+</html>
